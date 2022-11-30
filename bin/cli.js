bfee2c2310a0acc761df50cf4b04847c81e98c90
jusurkin <61032491+jusurkin@users.noreply.github.com>
2022-11-30 18:15:14 -0500

    Applied default values for sides, dice, and rolls

--- bin/cli.js
+++ bin/cli.js
@@ -5,6 +5,14 @@ import { roll } from "../lib/roll.js";
 
 const args = minimist(process.argv.slice(2))
 
-var data = roll(args.sides, args.dice, args.rolls);
+var sides = args.sides
+var dice = args.dice
+var rolls = args.rolls
+
+if (typeof sides == 'undefined') { sides = 6 }
+if (typeof dice == 'undefined') { dice = 2 }
+if (typeof rolls == 'undefined') { rolls = 1 }
+
+var data = roll(sides, dice, rolls);
 
 console.log(data);
