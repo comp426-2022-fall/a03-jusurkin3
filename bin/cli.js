52501ca661098659bc63390e2121929d18f2d4b6
jusurkin <61032491+jusurkin@users.noreply.github.com>
2022-11-30 18:02:44 -0500

    First test

---
+++ bin/cli.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import minimist from 'minimist'
+import { roll } from "../lib/roll.js";
+//const nroll = require("./lib/roll.js");
+
+const args = minimist(process.argv.slice(2))
+
+var data = roll(args.sides, args.dice, args.rolls);
+
+console.log(data);
